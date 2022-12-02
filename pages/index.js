7931c1fd82e33dd1d323e83a9d223b9f2474af9d
AzzPurnomo <zizmomoziz@gmail.com>
2022-12-02 20:31:04 +0700

    tentang kota malang v2

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
                             data-aos='fade-right'
                             data-aos-duration='1000'
                         >
-                            Sejarah Kota Malang
+                            Tentang Kota Malang
                         </h2>
                         <p
                             data-aos='fade-right'
@@ -78,19 +78,23 @@ export default function Home() {
                                 Pariwisata di Kota Malang cukup besar. Pada tahun 2016, tercatat jumlah wisatawan domestik di Kota Malang berjumlah 3.987.074 orang, 
                             sedangkan wisatawan mancanegara sejumlah 9.535 orang. Jumlah wisatawan tahun 2016 merupakan suatu lonjakan yang signifikan dari tahun 
                             sebelumnya. Dengan melihat bukti tersebut, pemerintah optimis jumlah kunjungan wisatawan, terutama mancanegara akan terus meningkat.
+                            <br/>
                                 Malang dikenal memiliki banyak sekali kampung tematik yang bernuansa pedesaan dan khas. Di antaranya, yang paling terkenal adalah Kampung 
                             Wisata Jodipan (Kampung Warna-Warni), kampung warna-warni pertama di Indonesia yang menjadi salah satu destinasi favorit di Kota Malang. 
                             Selain itu, ada juga Kampung Tridi yang terletak di seberang Kampung Warna-Warni yang terkenal akan karya seni mural di dinding-dinding 
                             perumahannya, seperti Haji Lane di Singapura. Kedua kampung tersebut dihubungkan oleh sebuah jembatan kaca. Keduanya merupakan tempat selfie 
                             favorit para wisatawan.
+                            <br/>
                                 Selain itu, ada juga kampung wisata di Kota Malang yang terkenal akan keramahan lingkungannya dan kehijauannya. Di antaranya adalah Kampung 
                             Glintung Go Green (3G) yang terletak di Purwantoro dan Kampung Bamboo Mewek di Tunjungsekar. Keramahan lingkungan di Kampung 3G dapat dilihat dari 
                             penuhnya kampung oleh tanaman. Kampung 3G pun merupakan kampung konservasi air pertama di Indonesia. Sedangkan, Kampung Bamboo Mewek dianggap ramah 
                             lingkungan karena penuh dengan pohon dan bambu serta merupakan kampung konservasi.
+                            <br/>
                                 Pada Februari 2015, Pemerintah Kota Malang meluncurkan sistem angkutan bus tingkat wisata bewarna hijau yang dinamai Bus Macyto, singkatan dari 
                             Malang City Tour yang disediakan secara gratis dan khusus untuk para wisatawan. Bus ini beroperasi di Kota Malang dari depan gedung DPRD Kota Malang 
                             dan rutenya melewati beberapa titik-titik penting di tiap sudut kota, di antaranya beberapa museum-museum penting, kawasan Jalan Ijen, wisata kuliner, 
                             dan sebagainya. Bus ini berkapasitas 40 penumpang dengan jatah keliling sebanyak tiga kali.
+                            <br/>
                                 Sarana penginapan untuk pariwisata di Kota Malang beragam, mulai dari hotel, apartemen, losmen, hingga rumah singgah yang tersebar di seluruh penjuru kota. 
                             Keberagaman ini didukung oleh kenyataan bahwa Malang merupakan tujuan wisata paling populer di Indonesia setelah Bali, Bandung, dan Yogyakarta. Hotel yang paling 
                             terkenal di kota adalah Hotel Tugu. Hal ini dikarenakan hotel sudah dikenal baik di kalangan wisatawan asing dan hotel ini sering memeanjakan pengunjungnya dengan 
